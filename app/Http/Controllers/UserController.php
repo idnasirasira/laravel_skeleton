febc2c316e6c9ed4d496c022d8938abc45f686e6
Aris Arisandi <idnasirasira@gmail.com>
2024-07-03 12:22:14 +0700

    Repository Service Interface Implementation

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\UserService;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = $this->userService->getAllUsers();
+        return response(200)->json($users);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        // Form Store
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $user = $this->userService->createUser($request->all());
+        return response()->json($user, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $user = $this->userService->getUserById($id);
+        return response()->json($user);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        // Form Edit
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $user = $this->userService->updateUser($id, $request->all());
+        return response()->json($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $this->userService->deleteUser($id);
+        return response()->json(null, 204);
+    }
+}
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\UserRepositoryInterface;
+use App\Models\User;
+
+class UserRepository implements UserRepositoryInterface
+{
+    public function all()
+    {
+        return User::all();
+    }
+
+    public function find($id)
+    {
+        return User::find($id);
+    }
+
+    public function create(array $data)
+    {
+        return User::create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $user = User::find($id);
+        $user->update($data);
+        return $user;
+    }
+
+    public function delete($id)
+    {
+        User::destroy($id);
+    }
+}
